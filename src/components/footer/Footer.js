3fe0053f981a688cd9c605380f2711968f2fb3b1
Ryan West <rbwest48@gmail.com>
2022-11-17 13:23:11 -0500

    Updated footer styling

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import MuiAppBar from '@mui/material/AppBar';
 import MuiTypography from '@mui/material/Typography';
+import MuiFavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
 const Footer = (props) => {
   return (
@@ -15,14 +16,31 @@ const Footer = (props) => {
         boxShadow: 'none',
         paddingBottom: '5px',
         paddingTop: '10px',
-        textAlign: 'center',
         ...props.styles,
       }}
       {...props.opts}
       id={props.id}
       className={props.classes}
     >
-      <MuiTypography> Made with love by Pendo Demo Engineering.</MuiTypography>
+      <MuiTypography
+        component="div"
+        sx={{
+          display: 'flex',
+          fontSize: '0.8rem',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        Made with
+        <MuiFavoriteBorderIcon
+          sx={{
+            color: '#0d88e6',
+            fontSize: '1.2rem',
+            padding: '0px 2px',
+          }}
+        ></MuiFavoriteBorderIcon>
+        by Pendo Demo Engineering.
+      </MuiTypography>
     </MuiAppBar>
   );
 };
