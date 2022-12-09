ecfc1296b3586f92c4c30f8afbbf6cb021a0ee11
Ryan West <rbwest48@gmail.com>
2022-12-09 11:56:19 -0500

    Added loading indicator for iframed content

--- src/common/forms/Embed.js
+++ src/common/forms/Embed.js
@@ -1,16 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import MuiBox from '@mui/material/Box';
 import MuiCircularProgress from '@mui/material/CircularProgress';
 
 const Embed = (props) => {
+  // Handle iframe loaded state
+  const [iframeLoaded, setIframeLoaded] = React.useState(false);
+  const handleIframeLoad = () => {
+    setIframeLoaded(true);
+  };
+
+  // Clear iframe loaded bool on src updated
+  React.useEffect(() => {
+    setIframeLoaded(false);
+  }, [props.src]);
+
   return (
-    <iframe
-      src={props.src}
-      title={props.title}
-      width="100%"
-      height="100%"
-    ></iframe>
+    <>
+      <MuiBox
+        sx={{
+          display: iframeLoaded ? 'initial' : 'none',
+        }}
+      >
+        <iframe
+          src={props.src}
+          title={props.title}
+          onLoad={handleIframeLoad}
+          width="100%"
+          height="100%"
+        ></iframe>
+      </MuiBox>
+      <MuiBox
+        sx={{
+          display: iframeLoaded ? 'none' : 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <MuiCircularProgress
+          size={200}
+          sx={{
+            color: 'rgb(225, 227, 230)',
+          }}
+        ></MuiCircularProgress>
+      </MuiBox>
+    </>
   );
 };
 
